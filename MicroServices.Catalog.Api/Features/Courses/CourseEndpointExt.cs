12323616ced6dc4c0423e7ce001048225c635ca5
Gokhan Kus <gkus1998@hotmail.com>
2024-12-09 14:43:00 +0300

    GetById Course

--- MicroServices.Catalog.Api/Features/Courses/CourseEndpointExt.cs
+++ MicroServices.Catalog.Api/Features/Courses/CourseEndpointExt.cs
@@ -1,6 +1,7 @@
 ﻿using MicroServices.Catalog.Api.Features.Categories.Create;
 using MicroServices.Catalog.Api.Features.Courses.Create;
 using MicroServices.Catalog.Api.Features.Courses.GetAll;
+using MicroServices.Catalog.Api.Features.Courses.GetById;
 
 namespace MicroServices.Catalog.Api.Features.Courses
 {
@@ -10,7 +11,8 @@ namespace MicroServices.Catalog.Api.Features.Courses
 		{
 			app.MapGroup("api/courses").WithTags("Courses")
 				.CreateCourseGroupItemEndpoint()
-				.GetAllCoursesGroupItemEndpoint();
+				.GetAllCoursesGroupItemEndpoint()
+				.GetByIdCourseGroupItemEndpoint();
 		}
 	}
 }
---
+++ MicroServices.Catalog.Api/Features/Courses/GetById/GetCourseByIdEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using MicroServices.Catalog.Api.Features.Courses.Dtos;
+
+namespace MicroServices.Catalog.Api.Features.Courses.GetById
+{
+	public record GetCourseByIdQuery(Guid Id) : IRequestByServiceResult<CourseDto>;
+	public class GetCourseByIdQueryHandler(AppDbContext context, IMapper mapper) : IRequestHandler<GetCourseByIdQuery, ServiceResult<CourseDto>>
+	{
+		public async Task<ServiceResult<CourseDto>> Handle(GetCourseByIdQuery request, CancellationToken cancellationToken)
+		{
+			var course = await context.Courses.FirstOrDefaultAsync(x=>x.Id == request.Id, cancellationToken);
+
+			if (course == null)
+			{
+				return ServiceResult<CourseDto>.Error("Course not found",
+					$"The course with id({request.Id}) was not found", HttpStatusCode.NotFound);
+			}
+
+			var category = await context.Categories.FindAsync(course.CategoryId, cancellationToken);
+			course.Category = category!;
+
+			var courseAsDto = mapper.Map<CourseDto>(course);
+			return ServiceResult<CourseDto>.SuccessAsOk(courseAsDto);
+		}
+	}
+	public static class GetCourseByIdEndpoint
+	{
+		public static RouteGroupBuilder GetByIdCourseGroupItemEndpoint(this RouteGroupBuilder group)
+		{
+			group.MapGet("/{id:guid}", async (Guid id, IMediator mediator)
+				=> (await mediator.Send(new GetCourseByIdQuery(id))).ToGenericResult())
+				.WithName("GetByIdCourse");
+
+			return group;
+		}
+	}
+}
