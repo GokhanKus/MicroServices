76d01e440aeccdf71ca647adfb05d17ce172bf6d
Gokhan Kus <gkus1998@hotmail.com>
2024-12-19 12:32:50 +0300

    AddBasketItemCommand/Validator.cs

---
+++ MicroServices.Basket.Api/Features/Baskets/AddBasketItem/AddBasketItemCommand.cs
@@ -0,0 +1,6 @@
+﻿using MicroServices.Shared;
+
+namespace MicroServices.Basket.Api.Features.Baskets.AddBasketItem
+{
+	public record AddBasketItemCommand(Guid CourseId, string CourseName, decimal CoursePrice, string? ImageUrl) : IRequestByServiceResult;
+}
