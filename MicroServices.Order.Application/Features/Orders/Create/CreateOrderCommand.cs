312f8c821db95d1975f830b049d129ad7c2bd76e
Gokhan Kus <gkus1998@hotmail.com>
2025-03-12 13:07:44 +0300

    CreateOrderCommandHandler.cs

--- MicroServices.Order.Application/Features/Orders/Create/CreateOrderCommand.cs
+++ MicroServices.Order.Application/Features/Orders/Create/CreateOrderCommand.cs
@@ -1,6 +1,8 @@
-﻿namespace MicroServices.Order.Application.Features.Orders.Create
+﻿using MicroServices.Shared;
+
+namespace MicroServices.Order.Application.Features.Orders.Create
 {
-	public record CreateOrderCommand(float? DiscountRate, AddressDto Address, PaymentDto Payment, List<OrderItemDto> OrderItems);
+	public record CreateOrderCommand(float? DiscountRate, AddressDto Address, PaymentDto Payment, List<OrderItemDto> OrderItems):IRequestByServiceResult;
 	public record AddressDto(string Province, string District, string Street, string ZipCode, string Line);
 	public record PaymentDto(string CardHolderName, string CardNumber, string Expiration, string CVC, decimal Amount);
 	public record OrderItemDto(Guid ProductId, string ProductName, decimal UnitPrice);
---
+++ MicroServices.Order.Application/Features/Orders/Create/CreateOrderCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using MicroServices.Order.Application.Contracts.Repositories;
+using MicroServices.Order.Domain.Entities;
+using MicroServices.Shared;
+using MicroServices.Shared.Services;
+using System.Net;
+
+namespace MicroServices.Order.Application.Features.Orders.Create
+{
+	public class CreateOrderCommandHandler(IGenericRepository<Guid, Domain.Entities.Order> orderRepo, IGenericRepository<int, Address> addressRepo, IIdentityService identityService)
+		: IRequestHandler<CreateOrderCommand, ServiceResult>
+	{
+		public Task<ServiceResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+		{
+			if(request.OrderItems is null || !request.OrderItems.Any())
+			{
+				return Task.FromResult(ServiceResult.Error("order items not found","in order must have at least one order item",HttpStatusCode.BadRequest));
+			}
+			//TODO: begin transaction
+			var newAddress = new Address
+			{
+				Province = request.Address.Province,
+				District = request.Address.District,
+				Street = request.Address.Street,
+				ZipCode = request.Address.ZipCode,
+				Line = request.Address.Line
+			};
+			addressRepo.Add(newAddress);
+			//TODO: unitOfWork.Commit()
+
+			var order = Domain.Entities.Order.CreateUnPaidOrder(identityService.UserId, request.DiscountRate, newAddress.Id);
+
+			foreach (var item in request.OrderItems)
+			{
+				order.AddOrderItem(item.ProductId, item.ProductName, item.UnitPrice);
+			}
+			orderRepo.Add(order);
+
+			var paymentId = Guid.Empty;
+			//TODO: Payment işlemler başlayacak
+			order.SetPaidStatus(paymentId);
+
+			orderRepo.Update(order);
+			//unitOfWork.Commit()
+			return Task.FromResult(ServiceResult.SuccessAsNoContent());
+		}
+	}
+}
