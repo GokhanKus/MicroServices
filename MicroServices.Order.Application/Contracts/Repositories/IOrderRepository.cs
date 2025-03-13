6f28a2fb1780956a6b3723253a27d464f2f3ecc6
Gokhan Kus <gkus1998@hotmail.com>
2025-03-13 10:44:41 +0300

    GetOrdersByUserId Method

--- MicroServices.Order.Application/Contracts/Repositories/IOrderRepository.cs
+++ MicroServices.Order.Application/Contracts/Repositories/IOrderRepository.cs
@@ -3,5 +3,6 @@
 	public interface IOrderRepository:IGenericRepository<Guid,Domain.Entities.Order>
 	{
 		//ilerde buraya yardımcı metotlar gelecek orn orderları almak ile ilgili
+		public Task<List<Domain.Entities.Order>> GetOrdersByUserId(Guid buyerId);
 	}
 }
--- MicroServices.Order.Persistence/Repositories/OrderRepository.cs
+++ MicroServices.Order.Persistence/Repositories/OrderRepository.cs
@@ -1,9 +1,13 @@
 ﻿using MicroServices.Order.Application.Contracts.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace MicroServices.Order.Persistence.Repositories
 {
 	public class OrderRepository(AppDbContext context) : GenericRepository<Guid, Domain.Entities.Order>(context), IOrderRepository
 	{
-	
+		public Task<List<Domain.Entities.Order>> GetOrdersByUserId(Guid buyerId)
+		{
+			return Context.Orders.Include(o=>o.OrderItems).Where(o => o.BuyerId == buyerId).OrderByDescending(o=>o.Created).ToListAsync(); 
+		}
 	}
 }
